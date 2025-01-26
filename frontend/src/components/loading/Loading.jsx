78d755ba08dec1f3b5d671c109908969850b8331
mandiptmg <mandiptamang125@gmail.com>
2025-01-26 18:18:57 +0545

    add ldrs package and update Loading component to use tailspin loader

--- frontend/src/components/loading/Loading.jsx
+++ frontend/src/components/loading/Loading.jsx
@@ -1,10 +1,11 @@
+import "ldrs/tailspin";
+
+
 const Loading = () => {
   return (
-    <div className="w-full fixed top-0 left-0  min-h-screen grid place-items-center">
+    <div className="w-full fixed top-0 left-0 bg-white z-[9999]  min-h-screen grid place-items-center">
       <div className="flex items-center gap-5">
-        <div
-          className="animate-spin  rounded-full h-10 w-10 border-4 border-cyan-500 p-2"
-        />
+        <l-tailspin size="40" stroke="5" speed="0.9" color="#06b6d4"></l-tailspin>
         Loading...
       </div>
     </div>
